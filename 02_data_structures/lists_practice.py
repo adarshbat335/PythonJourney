608dfbfcfb41879ef5291b860ef595bf7a186a22
adarshbat335 <avd335@gmail.com>
2025-08-22 17:27:30 +0530

    feat: Initial practice with Python lists

---
+++ 02_data_structures/lists_practice.py
@@ -0,0 +1,15 @@
+# Creating a list of my favorite fruits
+favorite_fruits = ["Apple", "Banana", "Cherry"]
+
+# Printing the whole list
+print("My favorite fruits are:", favorite_fruits)
+
+# Accessing a single item from the list (Python counts from 0)
+first_fruit = favorite_fruits[0]  # This gets the first item
+print("My absolute favorite is:", first_fruit)
+
+# Adding a new fruit to the end of the list
+favorite_fruits.append("Orange")
+
+# Printing the list again to see the change
+print("My updated list of fruits:", favorite_fruits)
